d31fabe5bfbc6a655c6921279770743fddfaab4b
ahmadjunioor <161430813+ahmadjunioor@users.noreply.github.com>
2024-06-13 15:55:10 +0700

    membuat isi void insert

--- tugas.cpp
+++ tugas.cpp
@@ -31,5 +31,30 @@ public:
     }
 
     void insert(string element) //insert a node in the binary seacrh tree
+    {
+        node* newNode = new node(element, nullptr,nullptr); //allocate memory for the new node
+        newNode->info = element; //assign value to the data field of the new node
+        newNode->leftchild = nullptr; //make the left child of the new node point to NULL
+        newNode->rightchild = nullptr; //make the right child of the new node point to NULL
+
+        node* parent = nullptr;
+        node* currentNode = nullptr;
+        search(element, parent, currentNode); //Locate the node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // if the parent is null (tree is empty)
+        {
+            ROOT = newNode; //mark the new node as ROOT
+            return; //exit
+        }
+
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the
+        {
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // if the value in the data field of the new node is greater than that
+        {
+            parent->rightchild = newNode; // make the right child of the parent point to the new node
+        }
+    }
 
 }
